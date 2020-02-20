4585738eebad8f0773ce76ee27801ed92c554a5c
Alice Post <alicep2322@gmail.com>
2020-02-20 09:40:11 -0700

    Setup - but with errors in testing related to node versions

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -2,9 +2,9 @@ import { expect } from "chai"
 import sinon from "sinon"
 
 describe("Word watch functions", () => {
-  describe("", () => {
-    it("", () => {
+  describe("On the word watch root / a user sees", () => {
+    it("the top word and it's count on the right side of the page", () => {
+      // expect(true).to.be.true;
     })
   })
 })
-
